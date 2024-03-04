2690d969be28678c21f649a467c846d1a335f778
sullis <github@seansullivan.com>
2024-03-04 15:42:53 -0800

    fix compile error

--- src/test/java/io/github/sullis/micronaut/playground/NettyTest.java
+++ src/test/java/io/github/sullis/micronaut/playground/NettyTest.java
@@ -4,15 +4,21 @@ package io.github.sullis.micronaut.playground;
 import io.micronaut.http.server.netty.*;
 import io.micronaut.http.server.netty.configuration.NettyHttpServerConfiguration;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
 
 @MicronautTest
 public class NettyTest {
+    @Inject
+    DefaultNettyEmbeddedServerFactory nettyEmbeddedServerFactory;
+
 
     @Test
     public void testNetty() {
         final var config = new NettyHttpServerConfiguration();
-        final var services = new DefaultNettyEmbeddedServerFactory();
-        final var server = new NettyHttpServer(config, services, true);
+        final var server = new NettyHttpServer(config, nettyEmbeddedServerFactory, true);
+        server.start();
+        server.stop();
+        server.close();
     }
 }
