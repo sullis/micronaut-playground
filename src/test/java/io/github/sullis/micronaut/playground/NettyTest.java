486bea78c922b315a9bcd6ea568e1bef81dd791e
sullis <github@seansullivan.com>
2024-04-26 09:44:01 -0700

    refactor NettyTest.java

--- src/test/java/io/github/sullis/micronaut/playground/NettyTest.java
+++ src/test/java/io/github/sullis/micronaut/playground/NettyTest.java
@@ -1,11 +1,17 @@
 
 package io.github.sullis.micronaut.playground;
 
+import io.micronaut.http.HttpVersion;
 import io.micronaut.http.server.netty.*;
 import io.micronaut.http.server.netty.configuration.NettyHttpServerConfiguration;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import io.netty.handler.logging.LogLevel;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @MicronautTest
 public class NettyTest {
@@ -16,9 +22,17 @@ public class NettyTest {
     @Test
     public void testNetty() {
         final var config = new NettyHttpServerConfiguration();
-        final var server = new NettyHttpServer(config, nettyEmbeddedServerFactory, true);
+        config.setLogLevel(LogLevel.INFO);
+        config.setUseNativeTransport(true);
+        config.setDefaultCharset(StandardCharsets.UTF_8);
+        config.setHttpVersion(HttpVersion.HTTP_2_0);
+
+        final var server = nettyEmbeddedServerFactory.build(config);
+
         server.start();
+        assertThat(server.isRunning()).isTrue();
         server.stop();
+        assertThat(server.isRunning()).isFalse();
         server.close();
     }
 }
