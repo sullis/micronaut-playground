6e96017bdfc079e03086439e6235496bfe9159d0
sullis <github@seansullivan.com>
2024-04-27 11:45:53 -0700

    validate compressed responses (#9)

---
+++ src/test/java/io/github/sullis/micronaut/playground/Constants.java
@@ -0,0 +1,7 @@
+package io.github.sullis.micronaut.playground;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Constants {
+    public static final String PAYLOAD = StringUtils.repeat("Hello", 1000);
+}
