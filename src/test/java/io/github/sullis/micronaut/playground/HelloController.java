f86aef32f1bcca8264f9b8f899bfe021d753f846
sullis <github@seansullivan.com>
2024-04-27 12:20:44 -0700

    response content type: text/plain

--- src/test/java/io/github/sullis/micronaut/playground/HelloController.java
+++ src/test/java/io/github/sullis/micronaut/playground/HelloController.java
@@ -2,10 +2,12 @@ package io.github.sullis.micronaut.playground;
 
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
 
 @Controller
 public class HelloController {
     @Get("/")
+    @Produces(value = "text/plain")
     public String hello() {
         return Constants.PAYLOAD;
     }
